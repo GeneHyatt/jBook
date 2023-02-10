787df3d775cbe978f3f781cc99abffa99e2447ac
Gene Hyatt <gene.hyatt@gmail.com>
2023-02-10 15:19:16 -0500

    More clean up and making components reusable.

--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,14 @@
 import ReactDOM from "react-dom";
-import {useState} from "react";
-import CodeEditor from "./components/code-editor";
-import Preview from "./components/preview";
-import bundle from "./bundler";
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
+import CodeCell from './components/CodeCell';
 
 const App = () => {
-    const [input, setInput] = useState('');
-    const [code, setCode] = useState('');
 
-    const onClick = async () => {
-        const output = await bundle(input);
-        setCode(output);
-    };
-
-
-
-    return <div>
-        <CodeEditor
-            initialValue={"a = 1"}
-            onChange={(value) => setInput(value)}
-        />
+    return (
         <div>
-            <button onClick={onClick}>Submit</button>
+            <CodeCell/>
         </div>
-        <Preview code={code} />
-    </div>
+    );
 };
 
 
